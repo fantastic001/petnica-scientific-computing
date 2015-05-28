bb0794ba2d66f2929731f485de5639f8a335aa71
Stefan Nožinić <stefan@lugons.org>
2015-05-28 16:21:49 +0200

    Euler modified

--- euler.py
+++ euler.py
@@ -3,21 +3,22 @@ import numpy as np
 
 import matplotlib.pyplot as plt 
 
-dt = 0.1
+dt = 0.001
 
-def dy(t):
-    return 2*t
+def dy(t, y):
+    return 2*y
 
 y = []
 t = np.arange(0, 20, dt)
 
 for n in t:
     if n == 0: 
-        y.append(0)
+        y.append(1)
         continue
-    y.append(y[-1] + dt*dy(n))
+    y.append(y[-1] + dt*dy(n, y[-1]))
 
 plt.plot(t,y)
-plt.plot(t, t**2)
+plt.plot(t, math.e**(2*t))
 plt.show()
-print(y)
+b
+#print(y)
