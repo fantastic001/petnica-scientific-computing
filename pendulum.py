5a07f00602e2e9a13caeff2178a0e85977fc05c1
Stefan Nožinić <stefan@lugons.org>
2015-05-29 17:04:32 +0200

    Change step and plot all angles

--- pendulum.py
+++ pendulum.py
@@ -9,7 +9,7 @@ m = []
 i = [] 
 g = 9.81 
 r = 1 
-dt = 1
+dt = 0.01
 theta = [] 
 w = [] 
 for k in range(n):
@@ -27,11 +27,22 @@ i = np.array(i)
 theta = np.array(theta) 
 
 y = []
-
-for t in range(2000):
+T = 20
+tt = np.arange(0, T, dt)
+for t in tt:
     y.append(theta) 
     theta, w = make_step(theta, dt, i, m, r, g, w)
 
-plt.plot(y, np.sin(y))
+Y =[] 
+
+for nn in range(n):
+    yyy = []
+    for time_period in y:
+        yyy.append(time_period[nn])
+    Y.append(yyy)
+
+for nn in range(n):
+    plt.figure()
+    plt.plot(tt, np.sin(Y[nn]))
 
 plt.show()
