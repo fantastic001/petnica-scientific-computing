f33e7fe14e4e034a525f5f3c2504729f1162c7d9
Stefan Nožinić <stefan@lugons.org>
2015-05-29 16:15:28 +0200

    Test fix

--- test.py
+++ test.py
@@ -1,20 +1,20 @@
 import unittest 
 
 import numpy as np 
-
+import numpy.testing as nptest
 from lib import * 
 
 class TestMakeMatrix(unittest.TestCase):
     
     def test_ones(self):
         expected = np.array([[1, -1, 0, 0, 0, 0], [0, 1, -1, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, -1, 0, 0], [0, 0, 0, 0, -1, 0], [0, 0, 0, 0, 0, -1]])
-        self.assertTrue(make_matrix([0, 0, 0], [1, 1, 1], 1).all(expected))
+        nptest.assert_equal(make_matrix([0, 0, 0], [1, 1, 1], 1), expected)
 
 
 class TestMakeBVector(unittest.TestCase):
     
     def test_basic(self):
-        self.assertEqual(make_b_vector([0, 0, 0], 0, [0, 0, 0]), np.array([0, 0, 0, 0, 0, 0]))
+        nptest.assert_equal(make_b_vector([0, 0, 0], 0, [0, 0, 0]), np.array([0, 0, 0, 0, 0, 0]))
 
 if __name__ == '__main__':
     unittest.main()
