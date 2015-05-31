ff9964cc798f88edd8503a291ffe7a318411fdf5
Stefan Nožinić <stefan@lugons.org>
2015-05-31 23:55:33 +0200

    Euler integrator and basic integrator as abstract class

---
+++ lib/integrators.py
@@ -0,0 +1,45 @@
+
+class BaseIntegrator(object):
+    
+    def __init__(self):
+        pass
+    def make_step(self, x, v, t, dt, a):
+        """
+        Makes one step during integration 
+
+        returns (x,v)
+        """
+        pass
+
+    def integrate(self, x0, v0, a, t, dt):
+        """
+        Integrates over time t 
+
+        returns (t, x, v) as list which are functions of time 
+        """
+        pass
+
+class EulerIntegrator(BaseIntegrator):
+
+    def make_step(self, x, v, t, dt, a):
+        w = v + dt*a(x,v,t)
+        s = x + w*dt
+        return (s,w)
+
+    def integrate(self, x0, v0, a, t, dt):
+        x = [] 
+        v = [] 
+        steps = int(t/dt)
+        if t/dt != steps:
+            raise ValueError("Specify dt such that it gives integer number of steps")
+        time = 0 
+        x.append(x0)
+        v.append(v0)
+        for step in range(steps):
+           time += dt
+           s,w = self.make_step(x[-1], v[-1], time, dt, a)
+           x.append(s)
+           v.append(w)
+        return (x, v)
+
+
--- test.py
+++ test.py
@@ -2,19 +2,50 @@ import unittest
 
 import numpy as np 
 import numpy.testing as nptest
-from lib import * 
+from lib.integrators import * 
 
-class TestMakeMatrix(unittest.TestCase):
+def acc_zero(x, v, t):
+    return 0
+
+class TestEulerIntegrator(unittest.TestCase):
     
-    def test_ones(self):
-        expected = np.array([[1, -1, 0, 0, 0, 0], [0, 1, -1, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, -1, 0, 0], [0, 0, 0, 0, -1, 0], [0, 0, 0, 0, 0, -1]])
-        nptest.assert_equal(make_matrix([0, 0, 0], [1, 1, 1], 1), expected)
+    def setUp(self):
+        self.integrator = EulerIntegrator()
 
-
-class TestMakeBVector(unittest.TestCase):
+    def test_constant(self):
+        x0 = 5
+        v0 = 0
+        x,v = self.integrator.integrate(x0, v0, acc_zero, 5, 0.01)
+        nptest.assert_almost_equal(x[-1], 5)
+        nptest.assert_almost_equal(v[-1], 0)
     
-    def test_basic(self):
-        nptest.assert_equal(make_b_vector([0, 0, 0], 0, [0, 0, 0]), np.array([0, 0, 0, 0, 0, 0]))
+    def test_linear(self):
+        x0 = 5
+        v0 = 10
+        x,v = self.integrator.integrate(x0, v0, acc_zero, 5, 0.01)
+        nptest.assert_almost_equal(x[-1], 55)
+        nptest.assert_almost_equal(v[-1], 10)
+
+
+class TestRK4Integrator(unittest.TestCase):
+    
+    def setUp(self):
+        self.integrator = RK4Integrator()
+
+    def test_constant(self):
+        x0 = 5
+        v0 = 0
+        x,v = self.integrator.integrate(x0, v0, acc_zero, 5, 0.01)
+        nptest.assert_almost_equal(x[-1], 5)
+        nptest.assert_almost_equal(v[-1], 0)
+    
+    def test_linear(self):
+        x0 = 5
+        v0 = 10
+        x,v = self.integrator.integrate(x0, v0, acc_zero, 5, 0.01)
+        nptest.assert_almost_equal(x[-1], 55)
+        nptest.assert_almost_equal(v[-1], 10)
+
 
 if __name__ == '__main__':
     unittest.main()
