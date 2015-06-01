837410dd23331fc16749800bda66450f3154d395
Stefan Nožinić <stefan@lugons.org>
2015-06-01 20:12:03 +0200

    Simulation of pendulum with euler's integrator

--- pendulum.py
+++ pendulum.py
@@ -1,7 +1,7 @@
-from lib import *
+from lib.integrators import EulerIntegrator
 import matplotlib.pyplot as plt
 import numpy as np 
-
+import numpy.linalg as linalg
 
 n = int(input("Enter N: "))
 
@@ -28,10 +28,49 @@ theta = np.array(theta)
 
 y = []
 T = 20
-tt = np.arange(0, T, dt)
-for t in tt:
-    y.append(theta) 
-    theta, w = make_step(theta, dt, i, m, r, g, w)
+
+def make_matrix(theta, i, R):
+    n = len(theta)
+    A = np.zeros([2*n, 2*n])
+
+    for k in range(n):
+        A[k][k] = 1
+    for k in range(n-1):
+        A[k][k+1] = - np.cos(theta[k+1])
+    for k in range(n):
+        A[n+k][n+k] = -i[k] / R
+    for k in range(n-1):
+        A[n+k][k+1] = np.sin(theta[k+1])
+    return A
+
+def make_b_vector(m, g, theta):
+    n = len(m)
+    b = np.zeros(2*n)
+    for i in range(n):
+        b[i] = np.cos(theta[i]) * g*m[i]
+        b[n + i] = np.sin(theta[i]) * g*m[i]
+    return b
+
+def make_solution(theta, i, m, r, g):
+    n = len(m)
+    A= make_matrix(theta, i, r)
+    b = make_b_vector(m, g, theta)
+    x = linalg.solve(A, b)
+    s = []
+    for k in range(n, 2*n):
+        s.append(x[k])
+    return np.array(s)
+
+def make_step(theta, dt, i, m, r, g, w):
+    v = w + dt*make_solution(theta, i, m, r, g)
+    s = theta + v*dt
+    return (s,v)
+
+def calc_acc(theta, w, t):  
+    return make_solution(theta, i, m, r, g)
+
+integrator = EulerIntegrator()
+t, y, w = integrator.integrate(theta, w, calc_acc, T, dt)
 
 Y =[] 
 
@@ -43,6 +82,13 @@ for nn in range(n):
 
 for nn in range(n):
     plt.figure()
-    plt.plot(tt, np.sin(Y[nn]))
+    plt.plot(t, np.sin(Y[nn]))
+
+angle = Y[-1]
+
+plt.figure()
+for aa in angle:
+    plt.plot(np.cos(aa), np.sin(aa), "o")
+
 
 plt.show()
